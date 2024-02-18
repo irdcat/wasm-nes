420a8c6be9d1f6f2bfb537e9af6fa1718a368037
irdcat <kmlkamilek@gmail.com>
2024-02-19 00:17:44 +0100

    Refactor overcomplicated PPU framebuffer #94

--- src/PpuFramebuffer.cpp
+++ src/PpuFramebuffer.cpp
@@ -2,27 +2,10 @@
 
 void PpuFramebuffer::setColor(unsigned x, unsigned y, u8 colorIndex)
 {
-    colorIndex &= COLOR_MASK;
-    const auto bitPosition = x * y * BITS_PER_COLOR;
-    const auto shiftOffset = bitPosition % BITS_PER_BYTE;
-    if(shiftOffset < SHIFT_OFFSET_THRESHOLD) {
-        buffer[bitPosition / BITS_PER_BYTE] |= colorIndex << shiftOffset;
-    } else {
-        buffer[bitPosition / BITS_PER_BYTE] |= colorIndex >> (shiftOffset - SHIFT_OFFSET_THRESHOLD);
-        buffer[(bitPosition + BITS_PER_COLOR) / BITS_PER_BYTE] |= colorIndex << (BITS_PER_BYTE + SHIFT_OFFSET_THRESHOLD - shiftOffset);
-    }
+    buffer[x + y * WIDTH] = colorIndex;
 }
 
 u8 PpuFramebuffer::getColor(unsigned x, unsigned y) 
 {
-    const auto bitPosition = x * y * BITS_PER_COLOR;
-    const auto shiftOffset = bitPosition % BITS_PER_BYTE;
-    auto result = 0;
-    if(shiftOffset < SHIFT_OFFSET_THRESHOLD) {
-        result = buffer[bitPosition / BITS_PER_BYTE] >> shiftOffset; 
-    } else {
-        result = buffer[bitPosition / BITS_PER_BYTE] << shiftOffset - SHIFT_OFFSET_THRESHOLD
-            | buffer[(bitPosition + BITS_PER_COLOR) / BITS_PER_BYTE] >> BITS_PER_BYTE + SHIFT_OFFSET_THRESHOLD - shiftOffset;
-    }
-    return result & COLOR_MASK;
+    return buffer[x + y * WIDTH];
 }
--- src/PpuFramebuffer.hpp
+++ src/PpuFramebuffer.hpp
@@ -16,15 +16,6 @@ class PpuFramebuffer
     private:
         static constexpr const unsigned WIDTH = 256;
         static constexpr const unsigned HEIGHT = 240;
-
-        static constexpr const unsigned BITS_PER_BYTE = 8;
-        static constexpr const unsigned BITS_PER_COLOR = 6;
-        static constexpr const unsigned SHIFT_OFFSET_THRESHOLD = 2;
-
-        static constexpr const unsigned PIXEL_COUNT = WIDTH * HEIGHT;
-        static constexpr const unsigned BUFFER_SIZE = PIXEL_COUNT * BITS_PER_COLOR / BITS_PER_BYTE;
-
-        static constexpr const unsigned COLOR_MASK = (1 << BITS_PER_COLOR) - 1;
-
+        static constexpr const unsigned BUFFER_SIZE = WIDTH * HEIGHT;
         std::array<u8, BUFFER_SIZE> buffer;
 };
