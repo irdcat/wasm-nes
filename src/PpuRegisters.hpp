6b3649fa807e1c38c687b1ac43e9c4143397ce54
irdcat <kmlkamilek@gmail.com>
2024-02-25 16:08:11 +0100

    Refactor: Register definitions

--- src/PpuRegisters.hpp
+++ src/PpuRegisters.hpp
@@ -1,10 +1,81 @@
 #pragma once
 
-#include "PpuCtrlRegister.hpp"
-#include "PpuMaskRegister.hpp"
-#include "PpuStatusRegister.hpp"
-#include "PpuScrollRegister.hpp"
-#include "PpuAddressRegister.hpp"
+#include "RegisterBit.hpp"
+
+union PpuCtrlRegister
+{
+    u8 raw;
+    RegisterBit<0, 2> baseNametableAddress;
+    RegisterBit<2> vramAddressIncrement;
+    RegisterBit<3> spritePatternTableAddress;
+    RegisterBit<4> backgroundPatternTableAddress;
+    RegisterBit<5> spriteSize;
+    RegisterBit<6> masterSlaveSelect;
+    RegisterBit<7> VBlankNmi;
+
+    operator u8&() { return raw; }
+
+    PpuCtrlRegister& operator=(u8 value) { raw = value; return *this; }
+};
+
+union PpuMaskRegister
+{
+    u8 raw;
+    RegisterBit<0> greyscale;
+    RegisterBit<1> showBg8;
+    RegisterBit<2> showSp8;
+    RegisterBit<3> showBg;
+    RegisterBit<4> showSp;
+    RegisterBit<5> emphasizeRed;
+    RegisterBit<6> emphasizeGreen;
+    RegisterBit<7> emphasizeBlue;
+
+    RegisterBit<3, 2> showBgSp;
+
+    operator u8&() { return raw; }
+
+    PpuMaskRegister& operator=(u8 value) { raw = value; return *this; }
+};
+
+union PpuStatusRegister
+{
+    u8 raw;
+    RegisterBit<5> spriteOverflow;
+    RegisterBit<6> spriteZeroHit;
+    RegisterBit<7> inVBlank;
+
+    operator u8&() { return raw; }
+
+    PpuStatusRegister& operator=(u8 value) { raw = value; return *this; }
+};
+
+union PpuScrollRegister
+{
+    u16 raw;
+    struct
+    {
+        u8 x;
+        u8 y;
+    };
+
+    operator u16&() { return raw; }
+
+    PpuScrollRegister& operator=(u16 value) { raw = value; return *this; }
+};
+
+union PpuAddressRegister
+{
+    u16 raw;
+    struct 
+    {
+        u8 low;
+        u8 high;
+    };
+    
+    operator u16&() { return raw; }
+
+    PpuAddressRegister& operator=(u16 value) { raw = value; return *this; }
+};
 
 struct PpuRegisters
 {
