f211b5370ff05d5faa648c14b75e33c8200d5a69
irdcat <kmlkamilek@gmail.com>
2024-02-26 22:20:35 +0100

    Introduce more PPU tests

--- build-tests.ps1
+++ build-tests.ps1
@@ -3,7 +3,4 @@ cmake --build build/tests
 
 $copyExclude = Get-ChildItem -Recurse .\build\tests
 Copy-Item .\tests\resources\nestest.log -Destination .\build\tests -Exclude $copyExclude
-Copy-Item .\tests\resources\nestest.nes -Destination .\build\tests -Exclude $copyExclude
-Copy-Item .\tests\resources\ppu_vbl_nmi.nes -Destination .\build\tests -Exclude $copyExclude
-Copy-Item .\tests\resources\ppu_open_bus.nes -Destination .\build\tests -Exclude $copyExclude
-Copy-Item .\tests\resources\test_ppu_read_buffer.nes -Destination .\build\tests -Exclude $copyExclude
+Copy-Item .\tests\resources\*.nes -Destination .\build\tests -Exclude $copyExclude
--- build-tests.sh
+++ build-tests.sh
@@ -4,7 +4,4 @@ cmake . -B build/tests -G "Unix Makefiles"
 cmake --build build/tests
 
 cp ./tests/resources/nestest.log ./build/tests/nestest.log
-cp ./tests/resources/nestest.nes ./build/tests/nestest.nes
-cp ./tests/resources/ppu_vbl_nmi.nes ./build/tests/ppu_vbl_nmi.nes
-cp ./tests/resources/ppu_open_bus.nes ./build/tests/ppu_open_bus.nes
-cp ./tests/resources/test_ppu_read_buffer.nes ./build/tests/test_ppu_read_buffer.nes
+cp ./tests/resources/*.nes ./build/tests
