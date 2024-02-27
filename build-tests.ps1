a268526f39c24e9e8e2d36468abd44a7b0248be8
Kamil Adamiec <kmlkamilek@gmail.com>
2024-02-27 23:02:18 +0100

    Replace CPU instructions test and temporarily disable faulty PPU tests

--- build-tests.ps1
+++ build-tests.ps1
@@ -2,5 +2,4 @@ cmake . -B build/tests -G "Unix Makefiles"
 cmake --build build/tests
 
 $copyExclude = Get-ChildItem -Recurse .\build\tests
-Copy-Item .\tests\resources\nestest.log -Destination .\build\tests -Exclude $copyExclude
 Copy-Item .\tests\resources\*.nes -Destination .\build\tests -Exclude $copyExclude
