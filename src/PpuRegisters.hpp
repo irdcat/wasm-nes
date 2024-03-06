0431ab0e23fc014cf6b718b5718c71ef02cc6a87
Kamil Adamiec <kmlkamilek@gmail.com>
2024-03-06 22:57:40 +0100

    Refactor: Manipulation of internal PPU address registers

--- src/PpuRegisters.hpp
+++ src/PpuRegisters.hpp
@@ -60,9 +60,8 @@ union PpuInternalRegister
     RegisterBit<12, 3, u32> fineY;
     RegisterBit<15, 3, u32> fineX;
     RegisterBit<0, 8, u32> vramAddressLow;
-    RegisterBit<8, 6, u32> vramAddressHigh;
+    RegisterBit<8, 7, u32> vramAddressHigh;
     RegisterBit<0, 14, u32> vramAddress;
-    RegisterBit<15, 1, u32> msbT;
 
     operator u16() { return raw; }
 
