92a6c850c687feb46ffc6f66a23d61840b85ddc1
Kamil Adamiec <kmlkamilek@gmail.com>
2024-08-04 13:15:36 +0200

    Install TailwindCSS and DaisyUI via NPM #175

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ emcmake cmake . -B build -G "Unix Makefiles"
 
 cp ./src/web/index.html ./build
 cp ./src/web/index.js ./build
+npm install
+npx tailwindcss -i ./src/web/index.css -o ./build/index.css --minify
 
 cd build
 make
