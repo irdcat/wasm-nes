8f84722ca17a55134abb1232928601db15b62845
Kamil Adamiec <kamil.adamiec97@gmail.com>
2024-08-25 01:49:16 +0200

    Additional CPU Instruction Tests #206

--- build-tests.sh
+++ build-tests.sh
@@ -3,7 +3,7 @@ set -ex
 emcmake cmake . -B build/tests -G "Unix Makefiles" -DTESTS:BOOLEAN=true
 
 mkdir -p ./build/tests/resources
-cp ./tests/resources/* ./build/tests/resources
+cp -r ./tests/resources/* ./build/tests/resources
 cd build/tests
 make
 cd ../..
