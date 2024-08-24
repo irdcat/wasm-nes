5d76bf7106d82d8709ba4a60e2fdc1815122edad
Kamil Adamiec <kamil.adamiec97@gmail.com>
2024-08-24 09:44:25 +0200

    Disable unreliable tests #194

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,23 +1,5 @@
 #include "CpuInstructionsTest.hpp"
-#include "PpuVblankNmiTest.hpp"
-#include "PpuOpenBusTest.hpp"
-#include "PpuReadBufferTest.hpp"
-#include "PpuPaletteRamTest.hpp"
-#include "PpuSpriteRamTest.hpp"
 #include "PpuDataAccessTest.hpp"
-#include "PpuVramAccessTest.hpp"
-#include "VBlankClearTimeTest.hpp"
-#include "SpriteHitBasicsTest.hpp"
-#include "SpriteHitAlignmentTest.hpp"
-#include "SpriteHitCornersTest.hpp"
-#include "SpriteHitFlipTest.hpp"
-#include "SpriteHitLeftClipTest.hpp"
-#include "SpriteHitRightEdgeTest.hpp"
-#include "SpriteHitScreenBottomTest.hpp"
-#include "SpriteHitDoubleHeightTest.hpp"
-#include "SpriteHitTimingBasicsTest.hpp"
-#include "SpriteHitTimingOrderTest.hpp"
-#include "SpriteHitEdgeTimingTest.hpp"
 
 #include "util/TestExecutor.hpp"
 
@@ -26,31 +8,7 @@ int main()
     TestExecutor testExecutor;
 
     testExecutor.registerIntegrationTest<CpuInstructionsTest>();
-    // Unsupported mapper
-    //testExecutor.registerIntegrationTest<PpuVblankNmiTest>();
-    testExecutor.registerIntegrationTest<PpuOpenBusTest>();
-    // Unsupported mapper
-    //testExecutor.registerIntegrationTest<PpuReadBufferTest>();
-    testExecutor.registerIntegrationTest<PpuPaletteRamTest>();
-    testExecutor.registerIntegrationTest<PpuSpriteRamTest>();
     testExecutor.registerIntegrationTest<PpuDataAccessTest>();
-    testExecutor.registerIntegrationTest<PpuVramAccessTest>();
-    testExecutor.registerIntegrationTest<VBlankClearTimeTest>();
-    /*
-    Disabled until the rework of test framework
-    
-    testExecutor.registerIntegrationTest<SpriteHitBasicsTest>();
-    testExecutor.registerIntegrationTest<SpriteHitAlignmentTest>();
-    testExecutor.registerIntegrationTest<SpriteHitCornersTest>();
-    testExecutor.registerIntegrationTest<SpriteHitFlipTest>();
-    testExecutor.registerIntegrationTest<SpriteHitLeftClipTest>();
-    testExecutor.registerIntegrationTest<SpriteHitRightEdgeTest>();
-    testExecutor.registerIntegrationTest<SpriteHitScreenBottomTest>();
-    testExecutor.registerIntegrationTest<SpriteHitDoubleHeightTest>();
-    testExecutor.registerIntegrationTest<SpriteHitTimingBasicsTest>();
-    testExecutor.registerIntegrationTest<SpriteHitTimingOrderTest>();
-    testExecutor.registerIntegrationTest<SpriteHitEdgeTimingTest>();
-    */
 
     return testExecutor.runAll();
 }
