9d739a187cc71ffccd1649db64029326526603fa
Kamil Adamiec <kmlkamilek@gmail.com>
2024-08-25 20:46:19 +0200

    CPU Interrupts Tests #204 (#217)

---
+++ tests/CpuInterruptsTest.cpp
@@ -0,0 +1,54 @@
+#include "util/BlarggRomTest.hpp"
+
+class CpuInterruptsTest : public BlarggRomTest
+{
+    protected:
+        CpuInterruptsTest() = default;
+
+        ~CpuInterruptsTest() = default;
+
+        void SetUp() override
+        {
+            BlarggRomTest::SetUp();
+        }
+
+        void TearDown() override
+        {
+            BlarggRomTest::TearDown();
+        }
+};
+
+TEST_F(CpuInterruptsTest, BranchDelaysIrq)
+{
+    GTEST_SKIP() << "Skipped due to lack of APU implementation";
+    auto result = run("resources/cpu_interrupts_v2/branch_delays_irq.nes");
+    ASSERT_EQ(0, result) << (result == 0x100 ? "Failed to load ROM" : readMessage());
+}
+
+TEST_F(CpuInterruptsTest, CliLatency)
+{
+    GTEST_SKIP() << "Skipped due to lack of APU implementation";
+    auto result = run("resources/cpu_interrupts_v2/cli_latency.nes");
+    ASSERT_EQ(0, result) << (result == 0x100 ? "Failed to load ROM" : readMessage());
+}
+
+TEST_F(CpuInterruptsTest, IrqAndDma)
+{
+    GTEST_SKIP() << "Skipped due to lack of APU implementation";
+    auto result = run("resources/cpu_interrupts_v2/irq_and_dma.nes");
+    ASSERT_EQ(0, result) << (result == 0x100 ? "Failed to load ROM" : readMessage());
+}
+
+TEST_F(CpuInterruptsTest, NmiAndBrk)
+{
+    GTEST_SKIP() << "Needs attention";
+    auto result = run("resources/cpu_interrupts_v2/nmi_and_brk.nes");
+    ASSERT_EQ(0, result) << (result == 0x100 ? "Failed to load ROM" : readMessage());
+}
+
+TEST_F(CpuInterruptsTest, NmiAndIrq)
+{
+    GTEST_SKIP() << "Skipped due to lack of APU implementation";
+    auto result = run("resources/cpu_interrupts_v2/nmi_and_irq.nes");
+    ASSERT_EQ(0, result) << (result == 0x100 ? "Failed to load ROM" : readMessage());
+}
