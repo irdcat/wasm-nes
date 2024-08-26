0aac69c5578a16d847dedc710f310cbec81d1801
Kamil Adamiec <kmlkamilek@gmail.com>
2024-08-26 17:01:44 +0200

    Additional documentation about tests in the comments (#223)

--- tests/util/BlarggRomTest.hpp
+++ tests/util/BlarggRomTest.hpp
@@ -4,6 +4,14 @@
 
 #include "SystemUnderTest.hpp"
 
+/**
+ * Test based on some of the test roms developed by blargg (Shay Green).
+ * 
+ * Tests save the test result at the memory location 0x6000.
+ * However to let the emulator know when the value at 0x6000 is a valid test result
+ * 0xDE 0xB0 0x61 are written to 0x6001 - 0x6003.
+ * At 0x6004 rom store a null-terminated string. As text is being produced, terminator is moved forward.
+ */
 class BlarggRomTest : public ::testing::Test
 {
     public:
--- tests/util/NesTestLogParser.hpp
+++ tests/util/NesTestLogParser.hpp
@@ -7,21 +7,27 @@
 
 #include "../../src/core/Types.hpp"
 
+/**
+ * Data contained withing single line of the nestest log.
+ */
 struct NesTestLogData
 {
-    u16 instructionAddress; // Address of the executed instruction
-    u8 opcode;              // Fetched instruction opcode
-    u8 registerA;           // Value of register A before instruction execution
-    u8 registerX;           // Value of register X before instruction execution
-    u8 registerY;           // Value of register Y before instruction execution
-    u8 registerP;           // Value of register P before instruction execution
-    u8 registerS;           // Value of Stack Pointer before instruction execution
-    unsigned cycleCounter;  // Cycles performed before before execution
-    unsigned lineNumber;    // Log line number
-    std::string line;
-    std::string previousLine;
+    u16 instructionAddress;   // Address of the executed instruction
+    u8 opcode;                // Fetched instruction opcode
+    u8 registerA;             // Value of register A before instruction execution
+    u8 registerX;             // Value of register X before instruction execution
+    u8 registerY;             // Value of register Y before instruction execution
+    u8 registerP;             // Value of register P before instruction execution
+    u8 registerS;             // Value of Stack Pointer before instruction execution
+    unsigned cycleCounter;    // Cycles performed before before execution
+    unsigned lineNumber;      // Log line number
+    std::string line;         // Current log line
+    std::string previousLine; // Previous log line
 };
 
+/**
+ * Parser used to parse lines of nestest log.
+ */
 struct NesTestLogParser
 {
     NesTestLogParser(std::ifstream stream);
